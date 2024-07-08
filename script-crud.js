84a7860de0f08f3a2df60f679de1225cf17b8b4d
GuiFelSS <guilhermeflp6@gmail.com>
2024-07-07 23:25:33 -0300

    finalizando a parte 04. Integrando com o cronômetro
    04. Integrando com o cronômetro, principais pontos aprendidos:
    
    adicionar e remover classes de elementos HTML para alterar sua aparência ou comportamento, como evidenciado pelo código que adiciona e remove a classe app__section-task-list-item-active;
    
    como lidar com um estado no JavaScript para rastrear a tarefa selecionada atualmente, usando variáveis como tarefaSelecionada e liTarefaSelecionada;
    
    lógica para permitir que os usuários cliquem e selecione/desselecione uma tarefa, e como responder a essas interações;
    
    criação e resposta a eventos personalizados, como o evento FocoFinalizado, que é usado para marcar uma tarefa como concluída;
    
    experiência do usuário ao fornecer feedback visual sobre a tarefa selecionada e a capacidade de marcar uma tarefa como concluída.

--- script-crud.js
+++ script-crud.js
@@ -3,8 +3,11 @@ const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textArea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list')
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+let tarefaSelecionada = null
+let liTarefaSelecionada = null
 
 function atualizarTarefas () {
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
@@ -32,7 +35,7 @@ function criarElementoTarefa(tarefa) {
 
     botao.onclick = () => {
         const novaDescricao = prompt("Qual é o novo nome da tarefa?")
-        console.log('Nova descriçaõ da tarefa: ', novaDescricao)
+        //console.log('Nova descriçaõ da tarefa: ', novaDescricao)
         if (novaDescricao !== null && novaDescricao !== "") {
             paragrafo.textContent = novaDescricao
             tarefa.descricao = novaDescricao
@@ -51,6 +54,23 @@ function criarElementoTarefa(tarefa) {
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
+
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active')
+            })
+            if (tarefaSelecionada == tarefa) {
+                paragrafoDescricaoTarefa.textContent=''
+                tarefaSelecionada = null
+                liTarefaSelecionada = null
+                return
+            }        
+            tarefaSelecionada = tarefa
+            liTarefaSelecionada = li
+            paragrafoDescricaoTarefa.textContent = tarefa.descricao
+            li.classList.add('app__section-task-list-item-active')
+    }
 
     return li
 }
@@ -93,3 +113,10 @@ btnCancelar.addEventListener('click', limparFormulario);
 //     formAdicionarTarefa.classList.toggle('hidden');
 // })
     
+document.addEventListener('focoFinalizado', () => {
+    if (tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled')
+    }
+})
